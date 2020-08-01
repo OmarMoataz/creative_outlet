cf17c5a9ba3cf8c8795e1b6ae9a069a44d44fc2b
Omar Moataz <omarmoataz@outlook.com>
2020-08-02 01:09:10 +0200

    Added roles to user serializer.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,8 +2,9 @@
 
 # Class that's responsible for returning the json object for posts.
 class UserSerializer < ApplicationSerializer
-  attributes :name, :email, :role_id
+  attributes :name, :email
   attribute :profile_image_url, key: :profileImageUrl
+  belongs_to :role
 
   def profile_image_url
     url_for(object.profile_image) if object.profile_image.attached?
